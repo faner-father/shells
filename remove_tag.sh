b4eb444c2fdd41b8876bce4a85af9ff62b1dfc30
faner father <fcq@fortunes.com.cn>
2015-04-29 17:38:49 +0800

    添加remove_tag.sh;完善tag.sh

---
+++ remove_tag.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+VERSION=
+
+if [ -z $1 ]
+then
+    echo "lack arguments:version"
+else
+    VERSION=$1
+fi
+
+MODULES=("ca" "lugia" "observation" "squirtle" "sturgeon" 
+        "sturgeon-client" "storage" "image" "monitor")
+ORIGIN_DIR=$PWD
+for m in ${MODULES[@]}
+do
+    cd $m
+    git tag -d $VERSION && git push origin --delete tag $VERSION && echo \
+    "remove $m $VERSION ok!"
+    cd $ORIGIN_DIR
+done
